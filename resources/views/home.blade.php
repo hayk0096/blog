e98206b05b5318859d9fa8ba508f5b79ac44a9bc
hayk <haykmargaryan0096@gmail.com>
2019-06-15 14:31:37 +0400

    changed dashboard name and displayed user name and email

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Profile</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,12 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div>
+                        {{ ucfirst(request()->user()->name) }}
+                    </div>
+                    <div>
+                        {{ request()->user()->email }}
+                    </div>
                 </div>
             </div>
         </div>
