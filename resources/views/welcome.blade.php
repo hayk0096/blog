26fb62ef1b892038d3e56fe380512fbfab657a7c
hayk <haykmargaryan0096@gmail.com>
2019-06-15 11:24:48 +0400

    added favicon for site and meta tag for cross,changed site title,background image and a few styles,customized navbar link items

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>Book Author</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,10 +13,14 @@
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
               crossorigin="anonymous">
 
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.png') }}">
+
+
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
+            body {
+                background: url( {{ asset('images/books/Literature-books-cover_2560x1600.jpg') }} ) no-repeat;
+                background-size: cover;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
@@ -52,7 +57,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #f7fcff;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -61,18 +66,29 @@
                 text-transform: uppercase;
             }
 
+            .links > a:hover,
+            .links > a.active {
+                text-decoration: underline;
+                color: #bfb3ff;
+            }
+
             .m-b-md {
                 margin-bottom: 30px;
             }
         </style>
+        @yield('css')
     </head>
     <body>
-{{--        <div class="flex-center position-ref full-height">--}}
+
+    @php $route = request()->route()->getName() @endphp
+
+
+    {{--        <div class="flex-center position-ref full-height">--}}
         <div class="position-ref full-height pt-3">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Profile</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
@@ -89,10 +105,16 @@
 {{--                </div>--}}
 
                 <div class="links">
-                    <a href="{{ route('authors.list') }}">Authors</a>
-                    <a href="{{ route('books.list') }}">Books</a>
+                    <a href="{{ route('books.list') }}" class="{{ preg_match('/(book*)/m', $route) ? 'active' : '' }}">Books</a>
+                    <a href="{{ route('authors.list') }}" class="{{ preg_match('/(author*)/m', $route) ? 'active' : '' }}">Authors</a>
+                </div>
+
+                <div id="content">
+                    @yield('content')
                 </div>
             </div>
         </div>
+
+    @yield('js')
     </body>
 </html>
