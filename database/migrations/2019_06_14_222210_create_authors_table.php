4de662457549cbe4a585722c4b6df966739aeff6
hayk <haykmargaryan0096@gmail.com>
2019-06-15 13:57:16 +0400

    added avatar column for authors

--- database/migrations/2019_06_14_222210_create_authors_table.php
+++ database/migrations/2019_06_14_222210_create_authors_table.php
@@ -18,6 +18,7 @@ class CreateAuthorsTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->text('bio');
+            $table->string('avatar');
             $table->timestamps();
         });
     }
