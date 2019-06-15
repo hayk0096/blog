464f4e20c2174b166b7076f800ef8213d2e34086
hayk <haykmargaryan0096@gmail.com>
2019-06-15 14:18:20 +0400

    added info text in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header d-flex justify-content-between">
+                    {{ __('Login') }}
+                    <span>Before add a book, please log in.</span>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
