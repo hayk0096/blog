dc284bb7bafb1acc49e99e65bfba75ab1bad90af
hayk <haykmargaryan0096@gmail.com>
2019-06-15 16:50:23 +0400

    created new stylesheet file and moved style from welcome file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,55 +27,8 @@
                 height: 100vh;
                 margin: 0;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #f7fcff;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .links > a:hover,
-            .links > a.active {
-                text-decoration: underline;
-                color: #bfb3ff;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
+        <link rel="stylesheet" href="{{ asset('css/book.css') }}">
         @yield('css')
     </head>
     <body>
@@ -109,8 +62,10 @@
                     <a href="{{ route('authors.list') }}" class="{{ preg_match('/(author*)/m', $route) ? 'active' : '' }}">Authors</a>
                 </div>
 
-                <div id="content">
-                    @yield('content')
+                <div class="container">
+                    <div id="content">
+                        @yield('content')
+                    </div>
                 </div>
             </div>
         </div>
